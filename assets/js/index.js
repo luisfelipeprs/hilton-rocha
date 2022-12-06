2406662c202c6eabb02266aa9fb9722aa86efdc5
Felipe Pereira <95257377+felipepx@users.noreply.github.com>
2022-12-06 14:08:38 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+!
 function(t, e) {
   "object" == typeof exports && "undefined" != typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define(e) : (t = "undefined" != typeof globalThis ? globalThis : t || self).bootstrap = e()
 }(this, (function() {
